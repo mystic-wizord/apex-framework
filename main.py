5d809948fd94dfa58bf9021f7f97e2ea7d34b579
wizord <wizord@pop-os.localdomain>
2022-01-26 23:44:01 +0200

    init apex with settings and main dir

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pathlib
+import os
+import pwd
 import click
 import chevron
 import json
@@ -10,6 +12,28 @@ def cli():
     Thank you for using Apex Framework!
     """
     pass
+
+@cli.command()
+def init():
+    click.echo(f"Initializing Apex!")
+
+    pathlib.Path(f'./apex-framework').mkdir(parents=True, exist_ok=True)
+
+    if pathlib.Path('/apex-framework/settings.json').is_file():
+        click.echo("It seems that apex has already been initialized!")
+    else:
+        work_dir = click.prompt("Please state where you would like your main working directory to be")
+
+        if not work_dir or not work_dir.strip() or len(work_dir) <= 0: 
+            work_dir = '..'
+
+        with open('./mustache/settings.mustache') as mustache:
+            settings_mustache = chevron.render(mustache, { 'work_dir': work_dir } )
+            settings_file = open('./apex-framework/settings.json', 'x')
+            settings_file.write(settings_mustache)
+            settings_file.close()
+
+            os.system(f'sudo mv "./apex-framework" "/home/{get_username()}/apex-framework" -i')
 
 
 @cli.command()
@@ -35,8 +59,10 @@ def create(name: str = None):
             rendered_mustache = chevron.render(data_file, { 'name': name, 'db_required': database_required, 'open_api_dir': open_api_dir } )
             click.echo(rendered_mustache)
 
-    pathlib.Path(f'../{name}/api-definitions').mkdir(parents=True, exist_ok=True)
-    app_definition_file = open(f'../{name}/application-manifest.json', 'x')
+    working_dir = get_working_dir()
+
+    pathlib.Path(f'{working_dir}/{name}/api-definitions').mkdir(parents=True, exist_ok=True)
+    app_definition_file = open(f'{working_dir}/{name}/application-manifest.json', 'x')
     app_definition_file.write(rendered_mustache)
     app_definition_file.close()
 
@@ -55,8 +81,19 @@ def purge(name: str = None):
         name = click.prompt("Please enter the name of the application to purge")
         click.echo(f"Purging {name}!")
 
-    pathlib.Path(f'../{name}').rmdir()
+    working_dir = get_working_dir()
 
+    pathlib.Path(f'{working_dir}/{name}').rmdir()
+
+def get_working_dir():
+    settings = open(f'/home/{get_username()}/apex-framework/settings.json',)
+   
+    # returns JSON object as a dictionary
+    data = json.load(settings)
+    return data['working-dir']
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
 
 if __name__ == "__main__":
     cli()
